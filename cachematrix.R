0bdd09b540d30b49ba51855da0942cf8ad93c2c9
Kyle Abraham <kyle.a.abraham@gmail.com>
2014-07-27 13:57:16 -0700

    Assignment 2 functions created

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a list that contains a function to cache to inverse of
+## a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function will:
+## Set values of a matrix
+## Retrieve matrix values
+## Set values of the inverse matrix
+## Retireve values of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # store matrix values in im
+    im <- NULL
+    
+    # Set matrix
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+    
+    # Retrieve matrix
+    ret <- function() x
+    
+    # Set inverse of matrix
+    setinverse<- function(inverse) im <<-inverse
+    
+    # Return inverse
+    returninverse <- function() im
+    
+    # List martix with functions
+    list(set = set, ret = ret,
+         setinverse = setinverse,
+         returninverse = returninverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+
+## cacheSolve wil compute the inverse of a pre-calculated matrix or return 
+## the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    im <- x$returninverse()
+    
+    # If inverse in calculated, return it
+    if (!is.null(im)) {
+        message("return cached inverse matrix")
+        return(im)
+    
+    #Else calculate inverse cache and return
+    } else {
+        im <- solve(x$ret())
+        x$setinverse(im)
+        return(im)
+    }
 }
+
